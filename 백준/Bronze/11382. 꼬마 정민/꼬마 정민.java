841104b6de7ee9ab107f6d00dfa037abce69eac2
김채연 <100480440+chaerish@users.noreply.github.com>
2024-10-31 02:43:23 +0900

    [Bronze V] Title: 꼬마 정민, Time: 108 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main {
+	public static void main (String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Long a = Long.parseLong(st.nextToken());
+		Long b = Long.parseLong(st.nextToken());
+		Long c = Long.parseLong(st.nextToken());
+		System.out.println(a+b+c); //억은 10의 9승 	
+	}
+
+}
