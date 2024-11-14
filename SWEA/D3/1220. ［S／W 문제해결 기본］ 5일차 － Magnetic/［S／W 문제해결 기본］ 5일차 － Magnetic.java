c309ca3eac3862356a241baddbbcb14f12989593
김채연 <100480440+chaerish@users.noreply.github.com>
2024-11-15 00:26:12 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 279 ms, Memory: 35,064 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
@@ -25,62 +25,67 @@
 //System.out.println(var);		       				   // 문자열 1개 출력하는 예제
 //System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
 /////////////////////////////////////////////////////////////////////////////////////////////
-import java.io.FileInputStream;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+
 
 /*
    사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
    이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
  */
-class Solution
-{
-	private static final int N = 100;
-	private static Deque<Boolean>[]dq ;
-	public static void main(String args[]) throws Exception{
-		//System.setIn(new FileInputStream("/Users/vecherish/Downloads/input.txt"));
-        Scanner sc = new Scanner(System.in);
-        for (int test=1; test<=10; test++) {
-        	String M = sc.nextLine();
-        	int answer = 0;
-        	dq = new ArrayDeque[100];
-        	for(int i =0; i<100; i++) {
-        		dq[i] = new ArrayDeque<Boolean>();
-        	}
-        	for(int i = 0; i<N;i++) {
-        		StringTokenizer st = new StringTokenizer(sc.nextLine());
-        		for(int j=0; j<N; j++) {
-        			String str = st.nextToken();
-                    if(str.equals("1"))
-                        dq[j].offer(true); //N to S
-                    else if(str.equals("2"))
-                        dq[j].offer(false); //S to N
-        		}
-        	}
-        	for(int i =0; i<100; i++) {
-        		while(!dq[i].isEmpty() && !dq[i].peekFirst()) { //false  ->위로 감 
-        			dq[i].pollFirst();
-        		}
-        		while(!dq[i].isEmpty() && dq[i].peekLast()) { //true  ->밑으로 가야됨 
-        			dq[i].pollLast();
-        		}
-        	}   	
-        	for(int i =0; i<100; i++) {
-        		if(dq[i].isEmpty()) {
-        			continue;
-        		}
-        		while(!dq[i].isEmpty()) {
-        			if(!dq[i].pollFirst()) {
-        				answer ++;
-        				while(!dq[i].isEmpty() && !dq[i].peekFirst()) { //교착지점 
-        					dq[i].pollFirst();
-            			}	
-        			}
-        		}
-        	}
-        	System.out.println("#"+test+" "+answer); 	
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+    private static final int N = 100;
+    private static Deque<Boolean>[] dq;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        for (int test = 1; test <= 10; test++) {
+            String M = br.readLine();
+            int answer = 0;
+            dq = new ArrayDeque[100];
+            for (int i = 0; i < 100; i++) {
+                dq[i] = new ArrayDeque<Boolean>();
+            }
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    String str = st.nextToken();
+                    if (str.equals("1"))
+                        dq[j].offer(true); // N to S
+                    else if (str.equals("2"))
+                        dq[j].offer(false); // S to N
+                }
+            }
+            for (int i = 0; i < 100; i++) {
+                while (!dq[i].isEmpty() && !dq[i].peekFirst()) { // false ->위로 감
+                    dq[i].pollFirst();
+                }
+                while (!dq[i].isEmpty() && dq[i].peekLast()) { // true ->밑으로 가야됨
+                    dq[i].pollLast();
+                }
+            }
+            for (int i = 0; i < 100; i++) {
+                if (dq[i].isEmpty()) {
+                    continue;
+                }
+                while (!dq[i].isEmpty()) {
+                    if (!dq[i].pollFirst()) {
+                        answer++;
+                        while (!dq[i].isEmpty() && !dq[i].peekFirst()) { // 교착지점
+                            dq[i].pollFirst();
+                        }
+                    }
+                }
+            }
+            sb.append("#").append(test).append(" ").append(answer).append("\n");
         }
-	}
+        System.out.println(sb);
+    }
 }
