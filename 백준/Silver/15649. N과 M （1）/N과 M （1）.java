1f87a8a59aea00c843ea84e1bd7b782a7130c640
김채연 <100480440+chaerish@users.noreply.github.com>
2024-12-27 18:13:39 +0900

    [Silver III] Title: N과 M (1), Time: 248 ms, Memory: 22344 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M = 0;
+    static boolean[] visit;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visit = new boolean[N + 1];
+        arr = new int[M];
+        recursive(0);
+        System.out.println(sb);
+    }
+
+    private static void recursive(int depth) {
+        if (depth == M) {
+            for (int num : arr) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 1; i <= N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i;
+                recursive(depth + 1);
+                visit[i] = false;
+            }
+        }
+    }
+}
