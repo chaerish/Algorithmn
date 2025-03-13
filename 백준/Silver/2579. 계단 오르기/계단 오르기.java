e7a442d90782863b67c1129a0243ffa70c002cbd
김채연 <100480440+chaerish@users.noreply.github.com>
2025-03-13 17:18:57 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -2,10 +2,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int[] dp;
-    static int[] arr;
 
     public static void main(String[] args) throws Exception {
+        int[] dp;
+        int[] arr;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         arr = new int[N + 1];
@@ -15,8 +15,7 @@ public class Main {
         if (N == 1) {
             System.out.println(arr[N]);
             return;
-        }
-        if (N == 2) {
+        } else if (N == 2) {
             System.out.println(arr[N - 1] + arr[N]);
             return;
         }
@@ -24,7 +23,8 @@ public class Main {
         dp[0] = 0;
         dp[1] = arr[1];
         dp[2] = arr[1] + arr[2];
-        for (int i = 3; i <= N; i++) { // 현재 입장에서 전에 O X or X O 만 보면 됨.
+        // O X or X O 만 보면 됨.
+        for (int i = 3; i <= N; i++) {
             dp[i] = Math.max(dp[i - 3] + arr[i - 1], dp[i - 2]) + arr[i];
         }
         System.out.println(dp[N]);
