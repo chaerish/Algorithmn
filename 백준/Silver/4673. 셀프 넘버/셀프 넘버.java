2f2100ba6067be976d6fd3edbfe62b543caf021a
김채연 <100480440+chaerish@users.noreply.github.com>
2025-03-17 20:36:52 +0900

    [Silver V] Title: 셀프 넘버, Time: 96 ms, Memory: 13992 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -1,15 +1,19 @@
-public class Main{
+public class Main {
     static int N = 10000;
     static StringBuilder sb = new StringBuilder();
     static boolean[] map = new boolean[N + 1];
 
     public static void main(String[] args) throws Exception {
         for (int i = 1; i <= N; i++) {
-            int key = divide(i, 0) + i;
-            if (key == 0 || key == i || key >= N) {
-                continue;
+            int num = i;
+            int value = num;
+            while (num > 0) {
+                value += (num % 10);
+                num /= 10;
             }
-            map[key] = true;
+            if (value <= N) {
+                map[value] = true;
+            }
         }
         for (int i = 1; i < N; i++) {
             if (!map[i]) {
@@ -17,28 +21,5 @@ public class Main{
             }
         }
         System.out.println(sb);
-    }
-
-    private static int divide(int i, int result) {
-        if (len(i) == 1) {
-            return result + i;
-        }
-        double divideNum = Math.pow(10, len(i) - 1); //100
-        int value = (int) (i / divideNum); //1
-        int remain = (int) (i % divideNum);
-        return divide(remain, result + value);
-    }
-
-    private static int len(int x) {
-        if (x / 10 == 0) {
-            return 1;
-        } else if (x / 100 == 0) {
-            return 2;
-        } else if (x / 1000 == 0) {
-            return 3;
-        } else if (x / 10000 == 0) {
-            return 4;
-        }
-        return 0;
     }
 }
