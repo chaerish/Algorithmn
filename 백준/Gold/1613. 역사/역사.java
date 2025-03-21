65a1ba97ddec55dae9ad2c202c3720b843a55f81
김채연 <100480440+chaerish@users.noreply.github.com>
2025-03-21 22:12:25 +0900

    [Gold III] Title: 역사, Time: 620 ms, Memory: 44416 KB -BaekjoonHub

---
+++ 백준/Gold/1613. 역사/역사.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, K = 0;
+    static int[][] dist;
+    static int MAX = (int) 1e9;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        dist = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                dist[i][j] = MAX;
+            }
+        }
+
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int before = Integer.parseInt(st.nextToken());
+            int after = Integer.parseInt(st.nextToken());
+            dist[before][after] = 1;
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        int F = Integer.parseInt(br.readLine());
+        for (int i = 0; i < F; i++) {
+            st = new StringTokenizer(br.readLine());
+            int before = Integer.parseInt(st.nextToken());
+            int after = Integer.parseInt(st.nextToken());
+            if (dist[before][after] != MAX) {
+                sb.append("-1").append("\n");
+            } else if (dist[after][before] != MAX) {
+                sb.append("1").append("\n");
+            } else {
+                sb.append("0").append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
