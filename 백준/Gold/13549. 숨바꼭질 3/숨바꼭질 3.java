6ab6bf578c587fa79e0a2be9952fa34aad93d578
김채연 <100480440+chaerish@users.noreply.github.com>
2025-05-02 16:23:19 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 132 ms, Memory: 17136 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -8,9 +8,9 @@ public class Main {
     /**
      * 순간이동 할 경우에는 0초 후에 2*X의 위치로 이동하게 됨.
      */
-    static int N, K = 0;
-    static int MAX = 100001;
-    static boolean[] visitied = new boolean[MAX];
+    static int N, K, next, time = 0;
+    static int[] move = {2, -1, 1};
+    static boolean[] visitied = new boolean[100001];
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,27 +18,30 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
+        int MIN = Integer.MAX_VALUE;
+
         Deque<int[]> dq = new ArrayDeque<>();
         dq.add(new int[]{N, 0});
         while (!dq.isEmpty()) {
             int[] curr = dq.pollFirst();
-            int idx = curr[0];
-            if (idx == K) {
+            if (curr[0] == K) {
                 System.out.println(curr[1]);
                 return;
             }
-            if (visitied[idx]) {
-                continue;
-            }
-            visitied[idx] = true;
-            if (idx * 2 < MAX) {
-                dq.addFirst(new int[]{idx * 2, curr[1]});
-            }
-            if (idx - 1 >= 0) {
-                dq.addLast(new int[]{idx - 1, curr[1] + 1});
-            }
-            if (idx + 1 < MAX) {
-                dq.addLast(new int[]{idx + 1, curr[1] + 1});
+            for (int i = 0; i < 3; i++) {
+                if (i == 0) {
+                    next = curr[0] * move[i];
+                } else {
+                    next = curr[0] + move[i];
+                }
+                if ((next >= 0 && next < visitied.length) && !visitied[next]) {
+                    visitied[next] = true;
+                    if (i == 0) {
+                        dq.addFirst(new int[]{next, curr[1]});
+                    } else {
+                        dq.addLast(new int[]{next, curr[1] + 1});
+                    }
+                }
             }
         }
     }
