cc3c84066358d01f76d8e74d4305f6c2dde433cf
김채연 <100480440+chaerish@users.noreply.github.com>
2025-05-06 17:52:46 +0900

    [Gold V] Title: 토마토, Time: 548 ms, Memory: 103700 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -5,63 +5,79 @@ import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] map;
-    static int M, N, total, riped = 0;
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
-    static Deque<int[]> dq = new ArrayDeque<>();
+    /**
+     * @name: 토마토
+     * @param: 상자 크기 나타내는 두 정수 M(가로길이), N(세로길이) 2<=M,N<=10^3 -> O(N^2)가능 1은 익은 토마토, 0은 익지 않은 토마토, -1는 토마토 없음 토마토가 익을지
+     * 토마토 익는 기준: 하루에 상하좌우
+     */
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int[][] tomato;
+    static int N, M, allCnt, notCnt, alreadyCnt = 0;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
 
-        map = new int[N][M];
+        tomato = new int[N][M];
+
+        Deque<int[]> dq = new ArrayDeque<>();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 int value = Integer.parseInt(st.nextToken());
-                map[i][j] = value;
-                if (value == 1) {
-                    riped++;
-                    dq.add(new int[]{i, j});
-                }
                 if (value != -1) {
-                    total++;
+                    allCnt++;
+                    if (value == 0) { //안익은거
+                        notCnt++;
+                    } else if (value == 1) { //익은거
+                        alreadyCnt++;
+                        dq.add(new int[]{i, j}); //익은것만 영향을 줄 수 있으니까
+                    }
                 }
+                tomato[i][j] = value;
             }
         }
-        // 모든 토마토가 이미 익어있다면 0 출력
-        if (total == riped) {
-            System.out.println(0);
+        if (alreadyCnt == allCnt) { //안익은거 + 익은거 == 모든 토마토
+            System.out.println(0); //이미 익어있음
             return;
         }
 
         int day = -1;
+
         while (!dq.isEmpty()) {
-            // 큐에 있는 모든 토마토에 대해 인접한 곳을 처리
             int size = dq.size();
             for (int s = 0; s < size; s++) {
                 int[] cur = dq.poll();
                 for (int i = 0; i < 4; i++) {
-                    int r = cur[0] + dr[i];
-                    int c = cur[1] + dc[i];
-                    if (isValid(r, c) && map[r][c] == 0) {
-                        map[r][c] = 1;
-                        riped++;
-                        dq.add(new int[]{r, c});
+
+                    int nx = cur[0] + dx[i];
+                    int ny = cur[1] + dy[i];
+                    //토마토 익히기! 
+                    if (isValid(nx, ny) && tomato[nx][ny] != -1 && tomato[nx][ny] == 0) {
+                        tomato[nx][ny] = 1;
+                        dq.add(new int[]{nx, ny});
                     }
                 }
             }
             day++;
         }
-        // 만약 모든 토마토가 익지 못했으면 -1 출력
-        System.out.println(total != riped ? -1 : day);
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (tomato[i][j] == 1) { //원래없었던거제외
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt == allCnt ? day : -1);
     }
 
-    private static boolean isValid(int r, int c) {
-        return r >= 0 && c >= 0 && r < N && c < M;
+    private static boolean isValid(int x, int y) {
+        return y >= 0 && y < M && x >= 0 && x < N;
     }
 }
