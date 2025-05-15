6908a1c1982719df39acb6982d07f29badd242fa
김채연 <100480440+chaerish@users.noreply.github.com>
2025-05-15 22:59:08 +0900

    [Gold V] Title: 평범한 배낭, Time: 168 ms, Memory: 53708 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,34 +1,40 @@
 import java.io.BufferedReader;
-import java.util.StringTokenizer;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String [] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		int [][] items = new int[N+1][2];
-		for(int i=1; i<N+1; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<2;j++) {
-				items[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		//dp 이차원 배열 선언
-		int [][] dp = new int[N+1][K+1];
-		for(int i=1; i<N+1;i++) {
-			for(int j=0; j<K+1;j++) { //무게가 0인경우가있음 .
-				dp[i][j] = dp[i-1][j]; //현재 아이템을 사용하지 않고 이전아이템의 최대가치를 이용
-				int w = items[i][0];
-				int v = items[i][1];
-				if(w<=j) { //현재 아이템의 무게보다 커야 쓸 수 있음.
-					dp[i][j]=Math.max(dp[i][j]
-									,dp[i-1][j-w]+v);
-				}
-			}
-		}
-		System.out.println(dp[N][K]);
-	}
+    static int N, K = 0; //N: 물품의 수, K: 준서가 버틸 수 있는 무게
+    static int[] w; //물건의 무게
+    static int[] v; //물건의 가치
+    static int[][] dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        w = new int[N + 1];
+        v = new int[N + 1];
+        dp = new int[N + 1][K + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            w[i] = Integer.parseInt(st.nextToken());
+            v[i] = Integer.parseInt(st.nextToken());
+        }
+        dp[0][0] = 0; //dp[i][j] -> i번째 아이템을 사용하거나, 사용하지 않거나 할 때 무게 j에서 최대 가치
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= K; j++) {
+                if (j >= w[i]) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        System.out.println(dp[N][K]);
+    }
+
 }
