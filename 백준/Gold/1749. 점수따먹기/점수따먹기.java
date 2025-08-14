349c4e23467cf75c3715c1cf2848f6e8cd8bceec
김채연 <100480440+chaerish@users.noreply.github.com>
2025-08-14 15:37:15 +0900

    [Gold IV] Title: 점수따먹기, Time: 1520 ms, Memory: 20444 KB -BaekjoonHub

--- 백준/Gold/1749. 점수따먹기/점수따먹기.java
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.java
@@ -3,9 +3,12 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] map;
+    /*
+
+     */
     static int N, M = 0;
     static int MAX = Integer.MIN_VALUE;
+    static int[][] map;
     static int[][] dp;
 
     public static void main(String[] args) throws Exception {
@@ -13,28 +16,27 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         map = new int[N][M];
         dp = new int[N + 1][M + 1];
-
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        //누적합 구하기
+        //누적합 쌓기
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= M; j++) {
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + map[i - 1][j - 1];
             }
         }
-        for (int r = 1; r < N; r++) {
-            for (int c = 1; c < M; c++) {
-                for (int i = 1; i + r - 1 <= N; i++) {
-                    for (int j = 1; j + c - 1 <= M; j++) {
-                        int value = dp[i + r - 1][j + c - 1] - dp[i + r - 1][j - 1] - dp[i - 1][j + c - 1]
-                                + dp[i - 1][j - 1];
+
+        for (int r = 1; r <= N; r++) {
+            for (int c = 1; c <= M; c++) {
+                for (int i = 1; r + i - 1 <= N; i++) {
+                    for (int j = 1; c + j - 1 <= M; j++) {
+                        int value = dp[r + i - 1][c + j - 1] - dp[r + i - 1][j - 1] - dp[i - 1][c + j - 1] + dp[i - 1][j
+                                - 1];
                         MAX = Math.max(value, MAX);
                     }
                 }
