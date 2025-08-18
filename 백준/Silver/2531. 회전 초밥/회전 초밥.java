a203a3e6fa2161c1d1d460c060e16eb73c9038b0
김채연 <100480440+chaerish@users.noreply.github.com>
2025-08-18 17:41:46 +0900

    [Silver I] Title: 회전 초밥, Time: 168 ms, Memory: 18712 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -2,45 +2,65 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- *
- */
 public class Main {
-    static int N, d, k, c, max, eat = 0;
+    /*
+    슬라이딩 윈도우를 이용해서 cnt
+    윈도우의 크기: k (연속해서 먹는 접시의 수)
+
+     */
     static int[] sushi;
+    static int d, k, c, N, cnt;
     static int[] arr;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        arr = new int[N + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); //접시의 수
-        d = Integer.parseInt(st.nextToken()); // 초밥가짓수
-        k = Integer.parseInt(st.nextToken()); //연속해서 먹을 수 있는
-        c = Integer.parseInt(st.nextToken()); //쿠폰 번호
-        arr = new int[N + k];
+
+        N = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+        arr = new int[N * 2];
         sushi = new int[d + 1];
+
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        for (int i = 0; i < k; i++) {
-            arr[i + N] = arr[i];
-        }
-        for (int i = 0; i < k; i++) {
-            if (++sushi[arr[i]] == 1) {
-                eat++;
-            }
-        }
-        max = eat + (sushi[c] == 0 ? 1 : 0);
-        for (int i = k; i < N + k - 1; i++) {
-            if (--sushi[arr[i - k]] == 0) {
-                eat--;
-            }
-            if (++sushi[arr[i]] == 1) {
-                eat++;
-            }
 
-            max = Math.max(max, eat + (sushi[c] == 0 ? 1 : 0));
+        for (int i = N; i < arr.length; i++) {
+            arr[i] = arr[i % N];
+        }
+
+        //슬라이딩 윈도우 초기값 세팅
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < k; i++) {
+            int cur = arr[i];
+            if (++sushi[cur] == 1) {
+                cnt++;
+            }
+        }
+        if (++sushi[c] == 1) {
+            cnt++;
+        }
+
+        max = cnt;
+
+        for (int i = 1; i <= N + k; i++) {
+            int remove = arr[i - 1];
+            int add = arr[i - 1 + k];
+
+            if (--sushi[remove] == 0) {
+                cnt--;
+            }
+            if (++sushi[add] == 1) {
+                cnt++;
+            }
+            max = Math.max(cnt, max);
+        }
+        if (++sushi[c] == 1) {
+            cnt++;
         }
         System.out.println(max);
     }
