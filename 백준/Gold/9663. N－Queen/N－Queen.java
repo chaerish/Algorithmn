46386917a02bd99dbf6b6dc31bb1dc56d5df61ee
김채연 <100480440+chaerish@users.noreply.github.com>
2025-08-18 18:30:19 +0900

    [Gold IV] Title: N-Queen, Time: 13028 ms, Memory: 17544 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,41 +1,42 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Main {
-    static int N, count = 0;
+public class Main{
     static int[] dx = {-1, -1, -1};
     static int[] dy = {-1, 0, 1};
+    static int N, cnt;
+    static int[][] arr;
     static boolean[][] visited;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
         visited = new boolean[N][N];
         backtracking(0);
-        System.out.println(count);
+        System.out.println(cnt);
     }
 
-    private static void backtracking(int depth) {
+    private static void backtracking(int depth) { // depth를 깊이 또는 행으로 봄. 퀸은 한 행에 하나만 놓을 수 있기 때문이다.
         if (depth == N) {
-            count++;
+            cnt++;
             return;
         }
-        for (int i = 0; i < N; i++) {
-            if (canPlace(depth, i)) {
-                visited[depth][i] = true;
+        for (int c = 0; c < N; c++) {
+            if (isValid(depth, c)) {
+                visited[depth][c] = true;
                 backtracking(depth + 1);
-                visited[depth][i] = false;
+                visited[depth][c] = false; //다시 되돌린다.(다음 선택지 탐색)
             }
         }
     }
 
-    private static boolean canPlace(int r, int c) {
-        //만약 Depth(r)이 3이면 그 위에 i-1,(i=1) i-2까지만 보면됨 따라서 i=1부터 r보다 작을때까지
-        for (int i = 1; i <= r; i++) {
+    private static boolean isValid(int r, int c) {
+        for (int i = 1; i <= r; i++) { //1부터 위쪽 모든 행을 탐색
             for (int j = 0; j < 3; j++) {
                 int nr = r + dx[j] * i;
                 int nc = c + dy[j] * i;
-                if (nc < N && nc >= 0 && visited[nr][nc]) {
+                if (nc < N && nc >= 0 && visited[nr][nc]) { //이미 방문한 선택지면 false
                     return false;
                 }
             }
