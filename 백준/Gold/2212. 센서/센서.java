551d80c3ca94890ce696e7bc0ad36ef3eb26adec
김채연 <100480440+chaerish@users.noreply.github.com>
2025-09-04 11:28:55 +0900

    [Gold V] Title: 센서, Time: 200 ms, Memory: 17212 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.java
+++ 백준/Gold/2212. 센서/센서.java
@@ -1,30 +1,36 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
-public class Main{
-	static int s[];
-	static int minus[];
-	public static void main(String []args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int K = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		s = new int [N];
-		minus = new int [N-1];
-		for(int i=0; i<s.length;i++) {
-			s[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(s);
-		for(int i=0; i<minus.length;i++) {
-			minus[i] = s[i+1] - s[i];
-		}
-		Arrays.sort(minus);
-		int answer = 0;
-		for(int i=0; i<N-K;i++) {
-			answer+=minus[i];
-		}
-		System.out.println(answer);
-	}
+public class Main {
+    static int N, K, answer = 0;
+    static Integer[] arr;
+    static Set<Integer> set = new HashSet<>();
+    static int[] diff;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            set.add(num);
+        }
+        arr = set.toArray(new Integer[0]);
+        Arrays.sort(arr);
+        diff = new int[set.size() - 1];
+        for (int i = 0; i < set.size() - 1; i++) {
+            diff[i] = arr[i + 1] - arr[i];
+        }
+        Arrays.sort(diff);
+        for (int i = 0; i < set.size() - K; i++) {
+            answer += diff[i];
+        }
+        System.out.println(answer);
+    }
 }
