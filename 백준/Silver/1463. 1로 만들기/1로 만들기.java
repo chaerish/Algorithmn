904062eecd6b450fd11a3214fed2df7b51a3ca40
김채연 <100480440+chaerish@users.noreply.github.com>
2025-09-10 18:00:12 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18180 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,23 +2,27 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int N = 0;
+    /*
+     1. X가 3으로 나눠떨어지면 3으로 나눔.
+     2. X가 2으로 나눠 떨어지면, 2로 나눔,
+     3 1를 뺀다.
+     */
+    static int N, count;
     static int[] dp;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        dp = new int[N + 1];
-
+        dp = new int[N + 1]; //dp[i] = i일때 최소 연산 숫자.
         dp[1] = 0;
-        
         for (int i = 2; i <= N; i++) {
             dp[i] = dp[i - 1] + 1;
-            if (i % 3 == 0) { //3,2 둘다로 나눠질 수 있기 때문에 else if 사용X
-                dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
-            } if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i / 2] + 1, dp[i]); //이미 dp[i]는 +1 이 된 값이므로 +1은 전 거에만 해줌 
-            } 
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
         }
         System.out.println(dp[N]);
     }
