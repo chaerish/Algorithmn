3b70fc3006a7786a5051abb787ac9126a354a56b
김채연 <100480440+chaerish@users.noreply.github.com>
2025-09-16 17:54:25 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -2,23 +2,25 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int N = 0;
-    static int[] dp;
+    static int N, T;
+    static int[] dp = new int[12];
     static StringBuilder sb = new StringBuilder();
+
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        dp = new int[12];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 4;
-        for (int i = 4; i <= 11; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-        }
-        for (int test = 0; test < N; test++) {
-            sb.append(dp[Integer.parseInt(br.readLine())]).append("\n");
+        T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            N = Integer.parseInt(br.readLine());
+            dp = new int[12];
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
+
+            for (int n = 4; n <= N; n++) {
+                dp[n] = dp[n - 1] + dp[n - 2] + dp[n - 3];
+            }
+            sb.append(dp[N]).append("\n");
         }
         System.out.println(sb);
     }
