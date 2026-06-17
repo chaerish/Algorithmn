e62607bd0dfc56ea80a9c7555ac796768c36771f
Kim Chaeyeon <100480440+chaerish@users.noreply.github.com>
2026-06-17 21:55:05 +0900

    [level 2] Title: 비밀 코드 해독, Time: 18.92 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -1,34 +1,6 @@
 class Solution {
-    int answer = 0;
     public int solution(int n, int[][] q, int[] ans) {
-        backtrack(1, new int[5], 0, n, q, ans);
+        int answer = 0;
         return answer;
     }
-    private void backtrack(int start, int [] selected, int depth, int n, int [][] q, int [] ans){
-        if(depth == 5){
-            if(isValid(selected,q,ans)){
-                answer++;
-            }
-            return;
-        }
-        
-        for(int i= start; i <=n; i++){
-            selected[depth] = i;
-            backtrack(i+1, selected, depth+1, n,q,ans);
-        }
-    }
-    
-    private boolean isValid(int [] selected, int [][] q, int [] ans){
-        for(int i=0; i<q.length; i++){
-            int matchCount = 0;
-            for(int s: selected){
-                for(int val: q[i]){
-                    if(s==val) matchCount++;
-                }
-            }
-            if(matchCount != ans[i]) return false;
-        }
-        return true;
-    }
-    
 }
